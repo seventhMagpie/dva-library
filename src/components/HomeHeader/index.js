55758ccc061c957fce93c326fa8ede32db9e525c
cduwx <1391708158@qq.com>
2020-07-28 11:09:18 +0800

    feat: 首页组件开发

--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -1,5 +1,26 @@
+const { Link } = require("dva/router")
+
 const HomeHeader = () => {
-  return (<div className="c-home-header">home-header</div>)
+
+  const menuList = [
+    {
+      to: '/book/borrow',
+      text: '看书'
+    }, {
+      to: '/book/return',
+      text: '还书'
+    }
+  ]
+
+  return (<div className="c-home-header">
+    <div>
+      {
+        menuList.map((curr) => {
+        return (<Link to={curr.to}>{curr.text}</Link>)
+        })
+      }
+    </div>
+  </div>)
 }
 
 export default HomeHeader
