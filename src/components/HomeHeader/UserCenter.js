448500cfce05643be5028452ca7e48de15f6f6b5
cduwx <1391708158@qq.com>
2020-07-28 18:27:57 +0800

    feat: 头部内容开发

---
+++ src/components/HomeHeader/UserCenter.js
@@ -0,0 +1,12 @@
+import classNames from 'classnames'
+
+import style from './index.less'
+import avatarDef from '../../assets/img/yay.jpg'
+
+const UserCenter = () => {
+  return (<div className={classNames(style['user-center'])}>
+    <img className={classNames(style.avatar, 'pointer')} src={avatarDef} alt="用户头像" />
+  </div>)
+}
+
+export default UserCenter
