6b94f91649a3148e0e6e3e44da78bcb2f23f2a35
cduwx <1391708158@qq.com>
2020-07-28 19:38:22 +0800

    feat: commit

---
+++ src/components/HomeHeader/Logo.js
@@ -0,0 +1,18 @@
+import style from './index.less'
+import { Link } from 'dva/router'
+import classNames from 'classnames'
+import logo from '../../assets/img/yay.jpg'
+
+const HomeHeader = () => {
+
+  console.log(style)
+
+  return (<div className={classNames(style['logo'])} flex-row="between">
+    <Link to={'/'} className={classNames(style['logo-link'], 'select-none pointer')} flex-row="center">
+      <img className={classNames(style['logo-icon'])} src={logo} alt="logo" />
+      <div className={classNames(style['logo-text'])}>魔都书城</div>
+    </Link>
+  </div>)
+}
+
+export default HomeHeader
--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -1,19 +1,19 @@
 import style from './index.less'
-import { Link } from 'dva/router'
 import classNames from 'classnames'
-import logo from '../../assets/img/yay.jpg'
+
+import Logo from './Logo'
+import UserCenter from './UserCenter'
 
 const HomeHeader = () => {
 
   console.log(style)
 
   return (<div className={classNames(style['c-home-header'])} flex-row="between">
-    <Link to={'/'} className={classNames(style['logo'], 'select-none pointer')} flex-row="center">
-      <img className={classNames(style['logo-icon'])} src={logo} alt="logo" />
-      <div className={classNames(style['logo-text'])}>魔都书城</div>
-    </Link>
     <div>
-      00
+      <Logo />
+    </div>
+    <div>
+      <UserCenter />
     </div>
   </div>)
 }
